ff7f3d7ff08249c7f963e0aaf43b16cb469c41ca
Breno Filipe de Andrade <78601321+brenofandrade@users.noreply.github.com>
2026-03-12 21:27:08 -0300

    Fix merge conflict in Home component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
 function Home() {
-<<<<<<< HEAD
+    
     return <h1>Bem-vindo ao TabNews</h1>
-=======
-    return <h1>Teste</h1>
->>>>>>> refs/remotes/origin/main
+    
 }
+
 
 export default Home;
